62475b2ca3fed2d5ac7d715192c21a6be6ebc472
Rachel Evans <rachel@rachelevans.org>
2024-10-03 07:54:32 +0200

    Add radial-wipe timing

--- workspaces/hexagons-browser/src/cellTimingFunctions.ts
+++ workspaces/hexagons-browser/src/cellTimingFunctions.ts
@@ -15,9 +15,24 @@ export const timingRadial: CellTimingFunction = () => {
   const scale = randomElementFrom([+1, -1]);
   return (item) =>
     Math.sqrt(
-      (originX - item.cell.offsetTop) ** 2 +
-        (originY - item.cell.offsetLeft) ** 2
+      (originX - item.cell.offsetLeft) ** 2 +
+        (originY - item.cell.offsetTop) ** 2
     ) * scale;
 };
 
-export default [timingZero, timingRandom, timingRadial];
+export const timingClock: CellTimingFunction = () => {
+  const originX = Math.random() * document.body.clientWidth;
+  const originY = Math.random() * document.body.clientHeight;
+  const scale = randomElementFrom([+1, -1]);
+  const start = Math.random() * 2;
+  return (item) => {
+    const dx = originX - item.cell.offsetLeft;
+    const dy = originY - item.cell.offsetTop;
+    let angle = Math.atan2(dy, dx) / Math.PI + 1; // range: (0, 2)
+    if (angle < start) angle += 2;
+
+    return angle * scale;
+  };
+};
+
+export default [timingZero, timingRandom, timingRadial, timingClock];
