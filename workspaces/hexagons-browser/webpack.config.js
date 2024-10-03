8abe1036efafd3ec182e0e3e7730987ae0aa4364
Rachel Evans <rachel@rachelevans.org>
2024-10-03 15:48:57 +0200

    Remove unused webpack plugin

--- workspaces/hexagons-browser/webpack.config.js
+++ workspaces/hexagons-browser/webpack.config.js
@@ -1,7 +1,6 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -16,14 +15,6 @@ const config = {
     open: true,
     host: "localhost",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
   module: {
     rules: [
       {
