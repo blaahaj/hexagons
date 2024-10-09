92e1967d82692a1e06dbc74ebedac4ec1b0296ce
Rachel Evans <rachel@rachelevans.org>
2024-10-09 06:53:18 +0200

    Split out 'move' effects

--- src/effects/transforms/compound.ts
+++ src/effects/transforms/compound.ts
@@ -4,6 +4,7 @@ import * as col from "./color";
 import * as con from "./content";
 import * as rot from "./rotations";
 import { CellTransformationFunction } from "./index";
+import { moveEverythingViaTranslate } from "./move";
 
 type F = CellTransformationFunction;
 
@@ -25,4 +26,7 @@ export default [
     rot.transformSingleRotateAll,
     col.transformNRandomColors
   ),
+  compound(moveEverythingViaTranslate, rot.transformSingleRotateZ),
+  compound(moveEverythingViaTranslate, rot.transformSingleRotateAll),
+  compound(moveEverythingViaTranslate, rot.transformIndependentRotateAll),
 ] as const;
