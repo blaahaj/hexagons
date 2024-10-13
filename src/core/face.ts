4bd49a93b09b02290c805e4d2fda29e8dd535978
Rachel Evans <rachel@rachelevans.org>
2024-10-13 20:57:56 +0200

    Split Text away from Face

--- src/core/face.ts
+++ src/core/face.ts
@@ -1,59 +1,11 @@
+import { Text } from "./text";
 import { maybeTransition } from "../lib/maybeTransition";
-
-export const TextPositions = ["top", "middle", "bottom"] as const;
-export type TextPosition = (typeof TextPositions)[number];
-
-interface TextPart {
-  readonly span: HTMLSpanElement;
-  text: string;
-}
-
-export class FaceText {
-  public readonly element: HTMLDivElement;
-  private readonly texts: [TextPart, TextPart];
-
-  constructor(textPosition: TextPosition) {
-    this.element = document.createElement("div");
-    this.element.setAttribute("class", `text ${textPosition}`);
-    const makeText = () => {
-      const span = document.createElement("span");
-      span.classList.add("alternate");
-      const text = "";
-      span.innerHTML = text; // Initialize with empty HTML content
-      return { span, text };
-    };
-    this.texts = [makeText(), makeText()];
-    this.texts[0].span.style.opacity = "1";
-    this.texts[1].span.style.opacity = "0";
-    this.element.append(...this.texts.map(t => t.span));
-  }
-
-  get text() {
-    return this.texts[0].span.innerHTML;
-  }
-
-  set text(value) {
-    if (value === this.text) return;
-    this.texts.reverse();
-    this.texts[0].span.innerHTML = value;
-    this.texts[0].span.style.opacity = "1"; // fade in
-    this.texts[1].span.style.opacity = "0"; // fade out
-  }
-
-  get color() {
-    return this.element.style.color;
-  }
-
-  set color(value: string) {
-    this.element.style.color = value;
-  }
-}
 
 export class Face {
   public readonly parts: {
-    top: FaceText;
-    middle: FaceText;
-    bottom: FaceText;
+    top: Text;
+    middle: Text;
+    bottom: Text;
   };
 
   private readonly _color: string | undefined;
@@ -66,9 +18,9 @@ export class Face {
 
   constructor(private readonly isBackface: boolean) {
     const parts = (this.parts = {
-      top: new FaceText("top"),
-      middle: new FaceText("middle"),
-      bottom: new FaceText("bottom"),
+      top: new Text("top"),
+      middle: new Text("middle"),
+      bottom: new Text("bottom"),
     });
 
     const element = document.createElement("div");
